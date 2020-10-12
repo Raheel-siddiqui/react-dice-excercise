f0850b882a2c5e46aacdb0f2c4d97434f5c08f2e
raheel-siddqui <raheelsiddiqui05@gmail.com>
2020-10-12 12:34:59 +0500

    Updates

---
+++ precache-manifest.2ec1056a2def7a3dd0c403568305f678.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89d92591a83bdb030f2ed0d627f5aa62",
+    "url": "/react-dice-excercise/index.html"
+  },
+  {
+    "revision": "e603a4b5df0a4438c555",
+    "url": "/react-dice-excercise/static/css/main.57b033b7.chunk.css"
+  },
+  {
+    "revision": "bbcff1db79cedf670ccd",
+    "url": "/react-dice-excercise/static/js/2.50b14857.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-dice-excercise/static/js/2.50b14857.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e603a4b5df0a4438c555",
+    "url": "/react-dice-excercise/static/js/main.4d3ebd23.chunk.js"
+  },
+  {
+    "revision": "c29dfa9af9c16e871de5",
+    "url": "/react-dice-excercise/static/js/runtime-main.42e1a82b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-dice-excercise/static/media/logo.5d5d9eef.svg"
+  }
+]);
